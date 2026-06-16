7d632de1265a43fc6a247830716a840136a9e413
Shradul <revenclaw26@gmail.com>
2026-06-16 15:46:10 +0530

    added some temporal features

--- src/data_processor.py
+++ src/data_processor.py
@@ -8,26 +8,82 @@ import os
 def load_data():
     races = pd.read_csv("data/raw/races.csv")
     results = pd.read_csv("data/raw/results.csv")
-    races = races[["raceId","year","circuitId"]]
+    races = races[["raceId","year","round","circuitId"]]
     results = results[["resultId","raceId","driverId","constructorId","grid","position"]]
     data = results.merge(races, on="raceId")
     data = data[data["position"] != "\\N"]
     data["position"] = data["position"].astype(int)
+    data = data.sort_values(by=["year","round"]).reset_index(drop=True)
     data.to_csv("data/processed/cleaned_data.csv", index=False)
 
-
-def random_forest():
+def process_data():
     data = pd.read_csv("data/processed/cleaned_data.csv")
-    X = data.drop("position", axis=1)
-    Y = data["position"]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    data = temporal_features(data)
+    train_data = data[data["year"] < 2022]
+    test_data = data[data["year"] >= 2022]
+    train_data.to_csv("data/processed/train_data.csv", index=False)
+    test_data.to_csv("data/processed/test_data.csv", index=False)
+    print("Data processed successfully")
+    features_to_drop = ["position", "raceId", "resultId", "round", "year"]
+
+    x_train = train_data.drop(columns = features_to_drop)
+    y_train = train_data["position"]
+    x_test = test_data.drop(columns = features_to_drop)
+    y_test = test_data["position"]
+
+
+    return x_train, y_train, x_test, y_test
+
+def temporal_features(data):
+    data["driver_avg_pos_last_3"] = (data.groupby("driverId")["position"]
+        .transform(lambda x: x.shift(1).rolling(3, min_periods=1).mean()))
+    data["constructor_avg_pos_last_3"] = (data.groupby("constructorId")["position"]
+        .transform(lambda x: x.shift(1).rolling(3, min_periods=1).mean()))
+
+    data["driver_avg_pos_last_5"] = (data.groupby("driverId")["position"]
+        .transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean()))
+    data["constructor_avg_pos_last_5"] = (data.groupby("constructorId")["position"]
+        .transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean()))
+
+    data["driver_avg_grid_last_3"] = (data.groupby("driverId")["grid"]
+        .transform(lambda x: x.shift(1).rolling(3, min_periods=1).mean()))
+    data["constructor_avg_grid_last_3"] = (data.groupby("constructorId")["grid"]
+        .transform(lambda x: x.shift(1).rolling(3, min_periods=1).mean()))
+
+    data["driver_avg_grid_last_5"] = (data.groupby("driverId")["grid"]
+        .transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean()))
+    data["constructor_avg_grid_last_5"] = (data.groupby("constructorId")["grid"]
+        .transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean()))
+
+    data["driver_avg_pos_last_3"] = data["driver_avg_pos_last_3"].fillna(11)
+    data["constructor_avg_pos_last_3"] = data["constructor_avg_pos_last_3"].fillna(11)
+
+    data["driver_avg_pos_last_5"] = data["driver_avg_pos_last_5"].fillna(11)
+    data["constructor_avg_pos_last_5"] = data["constructor_avg_pos_last_5"].fillna(11)
+
+    data["driver_avg_grid_last_3"] = data["driver_avg_grid_last_3"].fillna(11)
+    data["constructor_avg_grid_last_3"] = data["constructor_avg_grid_last_3"].fillna(11)
+
+    data["driver_avg_grid_last_5"] = data["driver_avg_grid_last_5"].fillna(11)
+    data["constructor_avg_grid_last_5"] = data["constructor_avg_grid_last_5"].fillna(11)
+
+    return data
+
+def random_forest(x_train, y_train, x_test, y_test):
     model = RandomForestRegressor()
-    model.fit(X_train, Y_train)
-    print("test score:", model.score(X_test, Y_test))
-    Y_pred = model.predict(X_test)
-    mae = mean_absolute_error(Y_test, Y_pred)
+    model.fit(x_train, y_train)
+    print("test score:", model.score(x_test, y_test))
+    y_pred = model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_pred)
     print("MAE:", mae)
+    importance_df = pd.DataFrame({
+    "feature": x_train.columns,
+    "importance": model.feature_importances_
+    })
+    importance_df = importance_df.sort_values("importance", ascending=False)
+    print(importance_df)
 
 load_data()
-random_forest()
+x_train, y_train, x_test, y_test = process_data()
+random_forest(x_train, y_train, x_test, y_test)
 print(os.getcwd())
